32ebb99ef25c226d6fc01eb63f687bd5afe974c2
Ayomided <adediji.d@yahoo.com>
2024-09-04 20:56:50 +0100

    dwc counts words now

--- dwc/main.go
+++ dwc/main.go
@@ -2,52 +2,94 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 )
 
 var (
-	words string
+	ytes  string
 	lines string
+	words string
 )
 
 func main() {
-	flag.StringVar(&words, "c", "", "number of bytes in a file")
+	flag.StringVar(&ytes, "c", "", "number of bytes in a file")
 	flag.StringVar(&lines, "l", "", "number of lines in a file")
+	flag.StringVar(&words, "w", "", "number of words in a file")
 	flag.Parse()
 
-	if words != "" {
-		ByteCount(words)
-	}
-	if lines != "" {
+	if ytes != "" {
+		ByteCount(ytes)
+	} else if lines != "" {
 		LineCount(lines)
+	} else if words != "" {
+		WordsCount(words)
 	}
+}
+
+func checkFile(filePath string) bool {
+	_, error := os.Stat(filePath)
+	//return !os.IsNotExist(err)
+	return !errors.Is(error, os.ErrNotExist)
 }
 
 func ByteCount(input string) {
-	content, err := os.ReadFile(input)
-	if err != nil {
-		fmt.Printf("%d", len(input))
+	isFile := checkFile(input)
+	if isFile {
+		content, err := os.ReadFile(input)
+		if err != nil {
+			fmt.Printf("%d", len(input))
+		}
+		fmt.Printf("%d %s", len(content), input)
+	} else {
+		fmt.Printf("%d %s", len(input), input)
 	}
-	fmt.Printf("%d %s", len(content), input)
+
 }
 
 func LineCount(input string) {
-	content, err := os.Open(input)
-	if err != nil {
-		fmt.Println(err)
+	isFile := checkFile(input)
+	if isFile {
+		content, err := os.Open(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer content.Close()
+
+		fileScanner := bufio.NewScanner(content)
+		fileScanner.Split(bufio.ScanLines)
+
+		var fileLines []string
+
+		for fileScanner.Scan() {
+			fileLines = append(fileLines, fileScanner.Text())
+		}
+
+		fmt.Printf("%d %s", len(fileLines), input)
+	} else {
+		fmt.Println("Hello")
 	}
-	defer content.Close()
+}
 
-	fileScanner := bufio.NewScanner(content)
-	fileScanner.Split(bufio.ScanLines)
+func WordsCount(input string) {
+	isFile := checkFile(input)
+	if isFile {
+		content, err := os.Open(input)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer content.Close()
 
-	var fileLines []string
+		fileScanner := bufio.NewScanner(content)
+		fileScanner.Split(bufio.ScanWords)
+		var fileWords []string
 
-	for fileScanner.Scan() {
-		fileLines = append(fileLines, fileScanner.Text())
+		for fileScanner.Scan() {
+			fileWords = append(fileWords, fileScanner.Text())
+		}
+
+		fmt.Printf("%d %s", len(fileWords), input)
 	}
-
-	fmt.Printf("%d %s", len(fileLines), input)
 }

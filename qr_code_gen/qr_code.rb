1fb6c803f0262a09f54005a5a275009e140439e4
Ayomided <adediji.d@yahoo.com>
2024-09-27 18:59:14 +0100

    encode numeric data

--- qr_code_gen/qr_code.rb
+++ qr_code_gen/qr_code.rb
@@ -41,7 +41,12 @@ end
 
 def determine_input_mode(input)
   if numeric?(input)
-    NUMERIC
+    mode = :numeric
+    char_count_indicator = encode_character_count(input, mode)
+    puts "Input: #{input}"
+    puts 'Mode: Numeric'
+    puts "Character Count: #{input.length}"
+    puts "Character Count Indicator (binary): #{char_count_indicator}"
   elsif alphanumeric?(input)
     mode = :alphanumeric
     char_count_indicator = encode_character_count(input, mode)
@@ -56,28 +61,43 @@ def determine_input_mode(input)
   end
 end
 
-# EC_LEVEL 15%
-# Version 4 Size 33 * 33
-# Mode Indicator Alphanumeric 0010
-# Character count binary
+def encode_alphanumeric_data(input)
+  data_array = input.chars.map { |char| ALPHANUMERIC_CHARS.find_index(char) }
+  encoded_bits = []
 
-input_string = 'HELLO CC WORLD'
-
-data_array = input_string.chars.map { |char| ALPHANUMERIC_CHARS.find_index(char) }
-encoded_bits = []
-
-data_array.each_slice(2) do |pair|
-  if pair.length == 2
-    puts "Pair: #{pair}"
-    pair_value = pair[0] * 45 + pair[1]
-    encoded_bits << pair_value.to_s(2).rjust(11, '0')
-  else
-    encoded_bits << pair[0].to_s(2).rjust(6, '0')
+  data_array.each_slice(2) do |pair|
+    if pair.length == 2
+      pair_value = pair[0] * 45 + pair[1]
+      encoded_bits << pair_value.to_s(2).rjust(11, '0')
+    else
+      encoded_bits << pair[0].to_s(2).rjust(6, '0')
+    end
   end
+
+  string_out = encoded_bits.join
+  puts "Data: #{string_out}"
 end
 
-string_out = encoded_bits.join
-puts "Data: #{string_out}"
+def encode_numeric_data(input)
+  encoded_bits = []
+  input.chars.each_slice(3) do |three|
+    if three.length == 3
+      value = three.join.to_i
+      encoded_bits << value.to_s(2).rjust(10, '0')
+    elsif three.length == 2
+      value = three.join.to_i
+      encoded_bits << value.to_s(2).rjust(7, '0')
+    elsif three.length == 1
+      value = three.join.to_i
+      encoded_bits << value.to_s(2).rjust(4, '0')
+    end
+  end
+  encoded_bits.join
+end
+
+input = '123456789'
+encoded_data = encode_numeric_data(input)
+puts "Encoded Numeric Data: #{encoded_data}"
 
 # 0110000101101111000110100010111000100010100011001110100100010100110111011111
 # 01100001011011110001101000101110001000101000110011101001000101001101110111110
